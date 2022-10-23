3fbc230849a0f9d6ec5cdfd1678f65d1e7331ff6
Faraz Ali <exactlyfaraz@gmail.com>
2022-10-23 17:31:57 +0500

    Initial Commit | App Setup

---
+++ src/config/apiConfig.js
@@ -0,0 +1,9 @@
+const apiConfig = {
+    TMDB_API_KEY: "49b8b10162d2158e5adcf1634f5ad76b",
+    TMDB_Language: "en-US",
+    imgBase: "https://image.tmdb.org/t/p/original/",
+    imgBaseMedium: "https://image.tmdb.org/t/p/w500/",
+    imgBaseSmall: "https://image.tmdb.org/t/p/w200/",
+}
+
+export default apiConfig;
