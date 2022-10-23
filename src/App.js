a7f86ed5fae9d897e0abe7d8bf3b5fd11e5aa0fb
Faraz Ali <exactlyfaraz@gmail.com>
2022-10-23 18:28:17 +0500

    Added Routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import Home from './components/Home/Home';
+import MovieList from './components/MovieList/MovieList';
+import MovieDetail from './components/MovieDetail/MovieDetail';
+import ErrorScreen from './components/Error/Error';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello Carma</h1>
+      <Router>
+        <header className="App-header">
+            <Navbar/>
+        </header>
+        <div className="container">
+        <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/movies" component={MovieList} />
+            <Route path="/movie/:id" component={MovieDetail} />
+            <Route component={ErrorScreen} />
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
