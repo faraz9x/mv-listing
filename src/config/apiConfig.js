3485248538d673ce8c759ef476ffebe4c11ec2bc
Faraz Ali <exactlyfaraz@gmail.com>
2022-10-24 05:36:16 +0500

    API config

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,9 +1,11 @@
 const apiConfig = {
+    Base_URL: "https://api.themoviedb.org/3",
     TMDB_API_KEY: "49b8b10162d2158e5adcf1634f5ad76b",
     TMDB_Language: "en-US",
     imgBase: "https://image.tmdb.org/t/p/original/",
     imgBaseMedium: "https://image.tmdb.org/t/p/w500/",
     imgBaseSmall: "https://image.tmdb.org/t/p/w200/",
 }
+//https://api.themoviedb.org/3/authentication/guest_session/new?api_key=49b8b10162d2158e5adcf1634f5ad76b
 
 export default apiConfig;
